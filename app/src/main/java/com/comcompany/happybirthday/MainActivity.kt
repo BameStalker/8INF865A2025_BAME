10bb089e4635dc15901830c31d798fbfa9e70d81
BameStalker <seexbuubakarbame@gmail.com>
2025-11-04 16:35:48 -0500

    Etape8

--- app/src/main/java/com/comcompany/happybirthday/MainActivity.kt
+++ app/src/main/java/com/comcompany/happybirthday/MainActivity.kt
@@ -3,11 +3,10 @@ package com.comcompany.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-//import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-//import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -33,17 +32,19 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Text(
-        modifier = modifier,
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp,
-    )
-    Text(
-        text = from,
-        fontSize = 36.sp
-    )
+    Column(modifier = modifier) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp
+        )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
