8700533febb992da172f3c91029eb0f02cbc114f
BameStalker <seexbuubakarbame@gmail.com>
2025-11-04 16:23:19 -0500

    Etape7

--- app/src/main/java/com/comcompany/happybirthday/MainActivity.kt
+++ app/src/main/java/com/comcompany/happybirthday/MainActivity.kt
@@ -32,12 +32,16 @@ class MainActivity : ComponentActivity() {
     }
 }
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Text(
         modifier = modifier,
         text = message,
         fontSize = 100.sp,
         lineHeight = 116.sp,
+    )
+    Text(
+        text = from,
+        fontSize = 36.sp
     )
 }
 
@@ -45,6 +49,6 @@ fun GreetingText(message: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Bame!")
+        GreetingText(message = "Happy Birthday Bame!" , from = "From Bou")
     }
 }
